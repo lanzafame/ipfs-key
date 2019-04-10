0cac766baafe04f33e00012030dcb5cdd976d665
Adrian Lanzafame <adrianlanzafame92@gmail.com>
2019-04-10 12:39:46 +1000

    make ipfs-key more script friendly

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/base64"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
@@ -13,6 +15,9 @@ import (
 func main() {
 	size := flag.Int("bitsize", 2048, "select the bitsize of the key to generate")
 	typ := flag.String("type", "RSA", "select type of key to generate (RSA or Ed25519)")
+	file := flag.Bool("f", false, "output public and private key to files, instead of stdout")
+	prvout := flag.String("prvout", "priv.key", "output file for private key")
+	pidout := flag.String("pidout", "peer.id", "output file for peer id")
 
 	flag.Parse()
 
@@ -27,13 +32,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Fprintf(os.Stderr, "Generating a %d bit %s key...\n", *size, *typ)
 	priv, pub, err := ci.GenerateKeyPair(atyp, *size)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	fmt.Fprintln(os.Stderr, "Success!")
 
 	pid, err := peer.IDFromPublicKey(pub)
 	if err != nil {
@@ -41,7 +44,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Fprintf(os.Stderr, "ID for generated key: %s\n", pid.Pretty())
+	if *file {
+		pidf, err := os.Create(*pidout)
+		if err != nil {
+			log.Fatalf("failed to open pid file: %v\n", err)
+		}
+		defer pidf.Close()
+		if _, err := pidf.WriteString(pid.Pretty()); err != nil {
+			log.Fatalf("failed to write pid to file: %v\n", err)
+		}
+	} else {
+		fmt.Fprintln(os.Stdout, pid.Pretty())
+	}
 
 	data, err := priv.Bytes()
 	if err != nil {
@@ -49,5 +63,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	os.Stdout.Write(data)
+	b64data := base64.StdEncoding.EncodeToString(data)
+
+	if *file {
+		privf, err := os.Create(*prvout)
+		if err != nil {
+			log.Fatalf("failed to open private key file: %v\n", err)
+		}
+		defer privf.Close()
+		if _, err := privf.WriteString(b64data); err != nil {
+			log.Fatalf("failed to write to private key file: %v\n", err)
+		}
+	} else {
+		fmt.Fprintln(os.Stdout, b64data)
+	}
 }
